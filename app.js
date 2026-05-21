992e379617fa211e0c94552a6f43d5b92f6fe2d3
basalduainmobiliaria-gif <basalduainmobiliaria@gmail.com>
2026-05-21 02:04:22 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,23 +1,20 @@
 console.log("BNI PRO iniciado");
 
 const SUPABASE_URL = "https://wqiqauksktmvzppctoqo.supabase.co";
+
 const SUPABASE_KEY = "sb_publishable_ktAaADb0EkTBBvPh6JMWyw_eGVnlEiO";
 
-async function obtenerPropiedades() {
-
-  const response = await fetch(
-    `${SUPABASE_URL}/rest/v1/propiedades`,
-    {
-      headers: {
-        apikey: SUPABASE_KEY,
-        Authorization: `Bearer ${SUPABASE_KEY}`,
-      },
-    }
-  );
-
-  const data = await response.json();
-
+fetch(`${SUPABASE_URL}/rest/v1/propiedades?select=*`, {
+  headers: {
+    apikey: SUPABASE_KEY,
+    Authorization: `Bearer ${SUPABASE_KEY}`
+  }
+})
+.then(response => response.json())
+.then(data => {
+  console.log("PROPIEDADES:");
   console.log(data);
-}
-
-obtenerPropiedades();
+})
+.catch(error => {
+  console.error(error);
+});
