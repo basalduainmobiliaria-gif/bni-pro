4511b464aa4c8241c080118892a54865fed76540
basalduainmobiliaria-gif <basalduainmobiliaria@gmail.com>
2026-05-21 01:38:16 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 console.log("BNI PRO iniciado");
+
+const SUPABASE_URL = "https://wqiqauksktmvzppctoqo.supabase.co";
+const SUPABASE_KEY = "TU_ANON_KEY";
+
+async function obtenerPropiedades() {
+
+  const response = await fetch(
+    `${SUPABASE_URL}/rest/v1/propiedades`,
+    {
+      headers: {
+        apikey: SUPABASE_KEY,
+        Authorization: `Bearer ${SUPABASE_KEY}`,
+      },
+    }
+  );
+
+  const data = await response.json();
+
+  console.log(data);
+}
+
+obtenerPropiedades();
